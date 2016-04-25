17be9fa4056441facddf8f4647da10caa469faff
Leng <lengzhang@live.com>
2016-04-25 15:14:26 -0700

    complete class Op

--- lab4.cpp
+++ lab4.cpp
@@ -8,16 +8,54 @@ class Base {
 		Base () { };
 		/* Pure Virtual Functions */
 		virtual double evaluate () = 0;
-}
+};
 
 class Op: public Base {
 	public:
 		/* Constructors */
 		Op () {};
+		Op (const int new_num) {
+			num = new_num;
+		};
 		~ Op () {};
-}
 
-class Mult: public Base
+		void setNum(const int new_num) {
+			num = new_num;
+		};
+		int getNum() {
+			return num;
+		};
+	private:
+		int num;
+};
+
+class Mult: public Base {
+	public:
+		/* Constructors */
+		Mult () {};
+		~ Mult () {};
+};
+
+class Add: public Base {
+	public:
+		/* Constructors */
+		Add () {};
+		~ Add () {};
+};
+
+class Sub: public Base {
+	public:
+		/* Constructors */
+		Sub () {};
+		~ Sub () {};
+};
+
+class Sqr: public Base {
+	public:
+		/* Constructors */
+		Sqr () {};
+		~ Sqr () {};
+};
 
 int main() {
 
