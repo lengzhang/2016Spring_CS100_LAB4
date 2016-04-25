eecd89d91f08c06b6399a5551f68e8b018fadfaa
FlynnMcG <fmcge001@ucr.edu>
2016-04-25 15:43:59 -0700

    Added mult evaluate
    Removed add,div,sub, sqr

--- lab4.cpp
+++ lab4.cpp
@@ -39,74 +39,15 @@ class Mult: public Base {
 			op2 = new_op2;
 		};
 		~ Mult () {};
-		double evaluate () {
-			
+		double evaluate () 
+		{
+			return op1->evaluate()*op2->evaluate();
 		};
 	private:
 		Base* op1;
 		Base* op2;
 };
 
-class Mult: public Base {
-	public:
-	/* Constructors */
-	Mult () {};
-	Mult (Base* op1, Base* op2) {};
-	~ Mult () {};
-	virtual double evaluate()
-	{
-		return op1/op2;
-	}
-	private:
-	Base* op1;
-	Base* op2;
-};
-
-class Add: public Base {
-	public:
-		/* Constructors */
-		Add (Base* op_1, Base* op_2) {op1=op_1;op2=op_2;};
-		~ Add () {};
-		virtual double evaluate()
-		{
-			return op1+op2;
-		}
-	private:
-		Base* op1;
-		Base* op2;
-};
-
-class Sub: public Base {
-	public:
-		/* Constructors */
-<<<<<<< Updated upstream
-		virtual double evaluate()
-		{
-			return op1-op2;
-		}
-=======
->>>>>>> Stashed changes
-		Sub (Base* op_1, Base* op_2) {op1=op_1;op2=op_2;};
-		~ Sub () {};
-			
-	private:
-		Base* op1;
-		Base* op2;	
-};
-
-class Sqr: public Base {
-	public:
-		/* Constructors */
-		Sqr (Base* op_1) {op1=op_1;};
-		~ Sqr () {};
-		virtual double evaluate()
-		{
-			return op1*op1;
-		}
-		
-	private:
-		Base* op1;
-};
 
 int main() {
 //	Op* num = new Op(7);
