2be792b49e8a5ceb5a5533879cfae06019ec815d
Leng <lengzhang@live.com>
2016-04-25 14:52:35 -0700

    created README file

---
+++ lab4.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+class Base {
+	public:
+		/* Constructors */
+		Base () { };
+		/* Pure Virtual Functions */
+		virtual double evaluate () = 0;
+}
+
+class Op() {
+
+}
+
+
+
+int main() {
+
+	return 0;
+}
